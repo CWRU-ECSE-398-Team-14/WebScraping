48b81e42258a76a57abc71a58672dda623994c3e
thunderpudding <Baconscythe@yahoo.com>
2020-11-13 09:55:01 -0500

    Add files via upload

---
+++ getCountySystems.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor 
+This is a temporary script file.
+"""
+import requests
+#import pandas
+from bs4 import BeautifulSoup
+import sys
+#import json
+#from pandas import DataFrame as df 
+
+ctid = sys.argv[1]
+
+URL = "https://www.radioreference.com/apps/db/?ctid=" + ctid
+pageInitial = requests.get(URL)
+soup = BeautifulSoup(pageInitial.text, "html.parser")
+
+
+finalSID_set = set()
+cid = ""
+sidTemp = "sid="
+p25table = soup.find_all('table', attrs ={'class':'rrtable content'}) #loop through this
+for table in p25table:
+    aTag = table.find_all('a')
+    for a in aTag:
+        checkp25 = 0
+        tempString = a.get('href')
+        tempIndex = tempString.find(sidTemp)
+        tempName = str(a)
+        tempName2begin = tempName.find(">")   
+        tempName2end = tempName.find("</a>")
+        finalSID = tempString[tempIndex + 4:] + ", "
+        tempSystemInfo = table.text
+        systemInfoStringBeginIndex = a.find("</b>")
+        systemInfoStringEndIndex = a.find("</td>") 
+        systemInfoString = tempSystemInfo[systemInfoStringBeginIndex:systemInfoStringEndIndex]   #these indexes don't always provide the correct info for checking if P25
+        if "Project 25" in systemInfoString:                                                    #if two systems in same table and first one is p25, it always shows that the 2nd is as well
+            checkp25 =1
+    #if statement here checking if SID known
+        if finalSID not in finalSID_set and checkp25 == 1:
+            finalSID_set.add(finalSID)
+            print(finalSID)
+            finalSystemName = tempName[tempName2begin+1:tempName2end]
+            print(finalSystemName + ", ")
+            checkp25 =0
+
---
+++ getSystemTalkGroups.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 17:40:54 2020
+
+@author: Bacon
+"""
+
+#edit so that it adds " before and after, as well as a , after
+#one script for county ID to list all system IDs, found in <table class="rrtable content">
+    #as (for example) <a href="/apps/db/?sid=7127">
+    #Commonwealth Of Massachusetts Interoperable Radio System (CoMIRS)</a>
+#one script for system ID to spit out the csv-style list of talkgroups
+#str(number)
+import requests
+import pandas
+from bs4 import BeautifulSoup
+import sys
+import json
+from pandas import DataFrame as df 
+sid = sys.argv[1]
+#sid = "7337"
+URL = 'https://www.radioreference.com/apps/db/?sid=' + sid + "&opt=all_tg#tgs"
+#URL = 'https://www.radioreference.com/apps/db/?sid=7337&opt=all_tg#tgs'
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.text, "html.parser")
+freqTable = soup.find('table', attrs ={'class':'rrtable w1p'})
+#tbody = freqTable.find('tbody')
+trs = freqTable.find_all('tr')
+row = ""
+for tr in trs:
+      tds = tr.find_all('td')
+      for td in tds:
+          row = row + " " + td.text + ", "
+          #print(td.text)
+      print(row[:-2])                         #UNCOMMENT THIS LINE to get talkgroups
+      row = "" 
+      
+          
+# with open('output.txt', 'w') as f:
+#     for tr in soup.find_all('tr')[2:]:
+#         tds = tr.find_all('td')
+#         f.write("Nome: %s, Cognome: %s, Email: %s\n" % \
+#               (tds[0].text, tds[1].text, tds[2].text))
+          
